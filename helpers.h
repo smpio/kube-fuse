c7b3510e73dfeb38f9144caa6df5014c2b37eee0
Dmitry Bashkatov <dbashkatov@gmail.com>
2021-04-03 04:00:42 +0300

    add include guard

--- helpers.h
+++ helpers.h
@@ -1,2 +1,9 @@
+#ifndef KF_HELPERS_H_
+#define KF_HELPERS_H_
+
+#include <stdlib.h>
+
 void** copy_ptr_array(void** source, size_t size);
 char** copy_str_ptr_array(char** source, size_t size);
+
+#endif // KF_HELPERS_H_
